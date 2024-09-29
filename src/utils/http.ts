5ffba6ca3707178c237d8dc6d12a30c4a097b780
hs0820 <gustn1029@gmail.com>
2024-09-29 23:34:58 +0900

    ssl error로 다시 http로 변경

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -6,7 +6,7 @@ export const fetchUploadImage = async (imageBlob: Blob) => {
   const formData = new FormData();
   formData.append("image", imageBlob, "image/jpeg");
 
-  const res = await fetch("https://43.202.81.79:8080/api/upload", {
+  const res = await fetch("http://43.202.81.79:8080/api/upload", {
     method: "post",
     body: formData,
   });
