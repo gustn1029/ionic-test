4267b16f97b3da50df79595a2c2f0a27285a5e6b
hs0820 <gustn1029@gmail.com>
2024-09-29 23:19:44 +0900

    통신 함수 추가 및 client 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./utils/http";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
---
+++ src/utils/http.ts
@@ -0,0 +1,19 @@
+import { QueryClient } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient();
+
+export const fetchUploadImage = async (imageBlob: Blob) => {
+  const formData = new FormData();
+  formData.append("image", imageBlob, "image/jpeg");
+
+  const res = await fetch("http://43.202.81.79:8080/api/upload", {
+    method: "post",
+    body: formData,
+  });
+
+  if(!res.ok) {
+    throw new Error('이미지 업로드에 실패했습니다.')
+  };
+
+  return res.json();
+};
